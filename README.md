732af7d6da988cdc586f8a27761ab765835e3ad5
reborg <reborg@reborg.net>
2024-04-02 13:41:14 +0100

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# protocols
+
+    $ clojure -M:run-m
---
+++ test/protocols/protocols_test.clj
@@ -0,0 +1,7 @@
+(ns protocols.protocols-test
+  (:require [clojure.test :refer :all]
+            [protocols.protocols :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
